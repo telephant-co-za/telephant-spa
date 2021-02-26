c182f7725ae240f42afccd6a75e829d447664643
warren-byron <warren.byron@gmail.com>
2021-02-26 10:27:00 +0200

    TransactionsFilterCard initial creation.

---
+++ src/components/TransactionsFilterCard/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+// Implement CSS Styles here...
+
+export default function TransactionsExportCard(props) {
+    //const classes = useStyles();
+
+    return (
+        <Card>
+            <CardHeader title="Filter" />
+            <CardContent>
+                    <Grid container>
+                        <Grid item>
+
+                        </Grid>
+                    </Grid>
+            </CardContent>
+        </Card>
+    )
+}
--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import TransactionsExportCard from "../src/components/TransactionsExportCard";
+import TransactionsFilterCard from "../src/components/TransactionsFilterCard";
 
 
 storiesOf("Transactions Page/TransactionsExportCard", module)
     .add("default", () => <TransactionsExportCard /> )
+
+storiesOf("Transactions Page/TransactionsFilterCard", module)
+    .add("default", () => <TransactionsFilterCard /> )
