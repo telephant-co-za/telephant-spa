89bfd2bae1a756416b9c33abc380f950bd0a56fd
warren-byron <warren.byron@gmail.com>
2021-02-26 10:14:39 +0200

    TransactionsExportCard created and working.

--- src/components/TransactionsExportCard/index.js
+++ src/components/TransactionsExportCard/index.js
@@ -3,3 +3,25 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+// Implement CSS Styles here...
+
+export default function TransactionsExport(props) {
+    //const classes = useStyles();
+
+    return (
+        <Card>
+            <CardHeader title="Export CSV" />
+            <CardContent>
+                    <Grid container>
+                        <Grid item>
+
+                        </Grid>
+                    </Grid>
+            </CardContent>
+        </Card>
+    )
+}
