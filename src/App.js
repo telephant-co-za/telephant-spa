d31686756c44e886ebe1a562fd9eb83bd5496bc8
warren-byron <warren.byron@gmail.com>
2021-02-27 14:03:26 +0200

    Tabs selectable and added initial routing.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from "react";
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './Theme'
 import TemplateHeaderNavbar from "./components/TemplateHeaderNavbar"
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <TemplateHeaderNavbar />
-      {[...new Array(121)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
+      <BrowserRouter>
+        <TemplateHeaderNavbar />
+        <Switch>
+          <Route exact path="/" component={() => <div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />Home</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/TemplateHeaderNavbar/index.js
+++ src/components/TemplateHeaderNavbar/index.js
@@ -1,11 +1,19 @@
-import React from "react";
-//import { makeStyles } from "@material-ui/styles";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography";
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
 
 import Logo from "../Logo"
+
+const useStyles = makeStyles({
+    toolbarContainer: {
+        width: "100%"
+    }
+})
 
 // Elevate App Bar effect from https://material-ui.com/components/app-bar/
 function ElevationScroll(props) {
@@ -23,13 +31,25 @@ function ElevationScroll(props) {
 
 
 export default function TemplateHeaderNavbar() {
+    const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (e, value) => {
+        setValue(value)
+    }
+
     return (
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position="fixed">
-                    <Toolbar disableGutters>
-<Logo/>
+                    <Toolbar  className={classes.toolbarContainer} disableGutters>
+                        <Logo/>
                         <Typography>TelephantCloud</Typography>
+                        <Tabs value={value} onChange={handleChange}>
+                            <Tab label="Home"/>
+                            <Tab label="Transactions"/>
+                            <Tab label="Beneficiaries"/>
+                        </Tabs>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
