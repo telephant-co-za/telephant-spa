7217bea694ac351ded7b3523fccf7e26c2bd8a58
warren-byron <warren.byron@gmail.com>
2021-03-08 09:48:45 +0200

    Applied a custom palette and other styling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,19 @@
 
+// .storybook/preview.js
+
+import React from 'react';
+
+import { addDecorator } from '@storybook/react';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import { muiTheme } from '../src/stylesheet';
+
+addDecorator((story) => (
+    <ThemeProvider theme={muiTheme}>{story()}</ThemeProvider>
+));
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// https://stackoverflow.com/questions/61827931/how-to-theme-material-ui-inside-storybook
